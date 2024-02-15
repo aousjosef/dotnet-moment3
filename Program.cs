ec197a0d295bec2cc6354f8187eb63d319dd2767
aousjosef <aoussabah@gmail.com>
2024-02-15 20:58:28 +0100

    disabled code for dyanmic Ipv4 for local publishing. current code works good locally and for Azure publishing

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using Moment_3.Data;
-using System.Net;
-using System.Net.Sockets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,26 +7,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+
+     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+
+    );
+
 
 // Dynamically configure Kestrel to bind to the first non-loopback IPv4 address on port 1010
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    var ipv4Address = Dns.GetHostEntry(Dns.GetHostName())
-        .AddressList
-        .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+//builder.WebHost.ConfigureKestrel(serverOptions =>
+//{
+//    var ipv4Address = Dns.GetHostEntry(Dns.GetHostName())
+//        .AddressList
+//        .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
 
-    if (ipv4Address != null)
-    {
-        serverOptions.Listen(ipv4Address, 1010);
-    }
-    else
-    {
-        // Fallback to loopback if no non-loopback IPv4 address is found
-        serverOptions.Listen(IPAddress.Loopback, 1010);
-    }
-});
+//    if (ipv4Address != null)
+//    {
+//        serverOptions.Listen(ipv4Address, 1010);
+//    }
+//    else
+//    {
+//        Fallback to loopback if no non - loopback IPv4 address is found
+//       serverOptions.Listen(IPAddress.Loopback, 1010);
+//    }
+//});
 
 var app = builder.Build();
 
@@ -36,8 +37,10 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
